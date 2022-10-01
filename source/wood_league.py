7673a9ba128f1cddf2174c9c53456e524e17731e
CodExorcist <squall.leonhart.xp2008@gmail.com>
2022-10-01 13:52:21 +0800

    Update wood_league.py

--- source/wood_league.py
+++ source/wood_league.py
@@ -1,6 +1,7 @@
 import sys
 import math
 from enum import Enum
+import collections
 
 # Bring data on patient samples from the diagnosis machine to the laboratory with enough molecules to produce medicine!
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
@@ -30,25 +31,48 @@ class State:
         self.action = func
         return    
 
+    def __str__(self):
+        return f"Current Index={self.current_state_index}, Next Index={self.next_state_index}"
+
     def execute(self):
         self.action()
 
 
 class SampleData:
     
-    def __init__(self, _id=-1, carrier=-1, health=0, cost=None):
+    def __init__(self, _id=-1, carrier=-1, health=0, cost=()):
         self.ID = _id
         self.health = health
         self.cost = cost     
-        self.carrier = -1        
+        self.carrier = carrier        
+
+    def __str__(self):
+        return f"{self.ID},{self.health},{self.cost},{self.carrier}"
+
+    def __repr__(self):
+        return f"SampleData({self.ID},{self.carrier},{self.health},{self.cost})"
+
+    def __eq__(self, other) -> bool:
+        return self.ID == other.ID
+
+    def __lt__(self, other):
+        return self.health < other.health
+
+    def __gt__(self, other):
+        return self.health > other.health
 
     def is_cloud_data(self)->bool:
         return self.carrier == -1
+
+    def total_cost(self)->int:
+        return sum(list(self.cost))
+
+    
         
 class Robot:
+    __next_plausible_sample = None
 
-    def __init__(self, location=None, storages={}, sample_datas=[], state=None):
-        self.location = location
+    def __init__(self, storages={}, sample_datas=[], state=None):
         self.storages = storages
         self.sample_datas = sample_datas
         self.state = state
@@ -57,15 +81,18 @@ class Robot:
     
     def goto_diagnosis(self)->bool:        
         print("GOTO DIAGNOSIS")
-        return False
+        self.goto_next_state()
+        return True
 
     def goto_molecules(self)->bool:
         print("GOTO MOLECULES")
-        return False
+        self.goto_next_state()
+        return True
 
     def goto_laboratory(self)->bool:
         print("GOTO LABORATORY")
-        return False
+        self.goto_next_state()
+        return True
 
     def connect(self, _id):
         print(f"CONNECT {_id}")
@@ -79,25 +106,35 @@ class Robot:
         return sum(self.storages.values()) == 10
 
     def which_cost_not_fulfill(self, sample)->str:
-        if sample.cost_a > self.storages["A"]: return "A"
-        elif sample.cost_b > self.storages["B"]: return "B"
-        elif sample.cost_c > self.storages["C"]: return "C"
-        elif sample.cost_d > self.storages["D"]: return "D"
-        elif sample.cost_ed > self.storages["E"]: return "E"
+        if sample.cost[0] > self.storages["A"]: return "A"
+        elif sample.cost[1] > self.storages["B"]: return "B"
+        elif sample.cost[2] > self.storages["C"]: return "C"
+        elif sample.cost[3] > self.storages["D"]: return "D"
+        elif sample.cost[4] > self.storages["E"]: return "E"
         else: return ""
 
 
     def is_sample_fulfill(self, sample):
 
-        if sample.cost_a > self.storages["A"]: return False
-        elif sample.cost_b > self.storages["B"]: return False
-        elif sample.cost_c > self.storages["C"]: return False
-        elif sample.cost_d > self.storages["D"]: return False
-        elif sample.cost_e > self.storages["E"]: return False
+        if sample.cost[0] > self.storages["A"]: return False
+        elif sample.cost[1] > self.storages["B"]: return False
+        elif sample.cost[2] > self.storages["C"]: return False
+        elif sample.cost[3] > self.storages["D"]: return False
+        elif sample.cost[4] > self.storages["E"]: return False
 
         return True
 
     # Robot Action --------------------------------------------------------------
+
+    def __take_sample(self, sample_data)->bool:
+        if self.is_sample_data_full(): 
+            return False
+
+        if sample_data.is_cloud_data():
+            self.connect(sample_data.ID)
+            self.sample_datas.append(sample_data)
+
+        return True 
     
     def __take_molecule(self, _id):
         self.connect(_id)
@@ -113,17 +150,18 @@ class Robot:
         self.storages["D"] -= sample.cost[3]
         self.storages["E"] -= sample.cost[4]
 
+    def sync_carrying_sample(self, all_samples):
+        for self_sample in self.sample_datas: 
+            sample = next(filter(lambda a: a == self_sample, all_samples))
+            self_sample.carrier = sample.carrier
+
+    def assign_next_plausible_sample(self, sample_data):
+        self.__next_plausible_sample = sample_data
+
     # Robot Main Action --------------------------------------------------------------
 
-    def take_sample(self, sample_data)->bool:
-        if self.is_sample_data_full(): 
-            return False
-
-        if sample_data.is_cloud_data():
-            self.connect(sample_data.ID)
-            self.sample_datas.append(sample_data)        
-
-        return True    
+    def take_next_sample(self)->bool:
+        return self.__take_sample(self.__next_plausible_sample)
 
     def consume_next_sample(self)->bool:
         for sample in self.sample_datas: 
@@ -160,7 +198,7 @@ class Robot:
         self.update_state(get_next_state_index(self.state.current_state_index))
         return
 
-    def execute_state(self)->bool:
+    def execute_state(self)->bool:        
         return self.state.action()
 
 """
@@ -173,25 +211,28 @@ Functions ----------------------------------------------------------------------
 
 def initialize_states():
     states.append(State(RobotState.START, RobotState.DIAGNOSIS, robot.goto_diagnosis))
-    states.append(State(RobotState.DIAGNOSIS, RobotState.DIAGNOSIS, robot.take_next_molecule))
+    states.append(State(RobotState.DIAGNOSIS, RobotState.DIAGNOSIS, robot.take_next_sample))
     states.append(State(RobotState.DIAGNOSIS, RobotState.MOLECULE, robot.goto_molecules))
     states.append(State(RobotState.MOLECULE, RobotState.MOLECULE, robot.take_next_molecule))
     states.append(State(RobotState.MOLECULE, RobotState.LABORATORY, robot.goto_laboratory))
     states.append(State(RobotState.LABORATORY, RobotState.LABORATORY, robot.consume_next_sample))
-    states.append(State(RobotState.LABORATORY, RobotState.LABORATORY), robot.goto_diagnosis)
+    states.append(State(RobotState.LABORATORY, RobotState.DIAGNOSIS, robot.goto_diagnosis))
     return
 
 def get_next_state_index(c_index)->RobotState:
     if c_index == RobotState.LABORATORY: return RobotState.DIAGNOSIS
-    
-    return c_index + 1
+    return RobotState(c_index.value + 1)
 
 
 def get_next_state(c_index, n_index)->State:
+    print(f"c={c_index}, n={n_index}", file=sys.stderr, flush=True)
     state = next(filter(lambda s: s.current_state_index == c_index and 
                                   s.next_state_index == n_index, states))    
+
     return state
 
+def find_next_plausible_sample(sample_datas)->SampleData:
+    return max([sample for sample in sample_datas if sample.is_cloud_data()])
 
 """
 End Functions ----------------------------------------------------------------------
@@ -202,7 +243,11 @@ Game ---------------------------------------------------------------------------
 """
 
 states = []
-robot = Robot()
+storages = { "A":0, "B":0, "C":0, "D":0, "E":0 }
+
+robot = Robot(storages=storages)
+initialize_states()
+robot.state = states[0]
 
 project_count = int(input())
 for i in range(project_count):
@@ -253,7 +298,12 @@ while True:
         sample = SampleData(sample_id, carried_by, health, 
                             (cost_a, cost_b, cost_c, cost_d, cost_e))
         all_samples.append(sample)
-    
+
+    print(collections.Counter([s.health for s in all_samples]), file=sys.stderr, flush=True)
+    robot.sync_carrying_sample(all_samples)
+    n_sample = find_next_plausible_sample(all_samples)
+    robot.assign_next_plausible_sample(n_sample)
+
     while not robot.execute_state(): 
         robot.goto_next_state()
         continue
